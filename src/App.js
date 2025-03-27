74c2c0258b28b4490b86a21b7f1e0307b69e28e9
Ahmad Owais <okams7@gmail.com>
2025-03-28 02:59:10 +0300

    Adding new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Home from './home';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1>
-      <p>{10}</p> // ستعرض الرقم 10
-      <p>{"مرحبًا بالمطورين"}</p> // ستعرض النص
-      <p>{[1, 2, 3, 4, 5]}</p> // ستعرض الأرقام كمصفوفة متصلة
-      <p>{Math.random() * 10}</p> // ستعرض رقمًا عشوائيًا في كل تحديث
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>مدونة احمد</h1>
+      <div className="links">
+        <a href="/">الرئيسية</a>
+        <a href="/create">مدونة جديدة</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
