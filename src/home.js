f3b5cb93ae9e66797605df717801220168a4e50f
Ahmad Owais <okams7@gmail.com>
2025-03-28 21:05:09 +0300

    State

--- src/home.js
+++ src/home.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 const Home = () => {
-    const handleClick = (name, e) => {
-        console.log("Hello " + name);
-        console.log(e.target)
+
+    const [name, setName] = useState("Sami");
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("Ali");
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>الصفحة الرئيسية</h2>
-            <button onClick={(e) => handleClick("Ahmad", e)}>Click me</button>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Change info</button>
         </div>
     );
 }
