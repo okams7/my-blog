396d258e45ba05f2a7946e92944e224e2abfac94
Ahmad Owais <okams7@gmail.com>
2025-04-24 10:42:40 +0300

    Fetch data

--- src/home.js
+++ src/home.js
@@ -1,33 +1,25 @@
 import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { id: 1, title: "مدونتي الاولى", body: "هذه بعض من الكلمات ..", auther: "سالم" },
-        { id: 2, title: "السراب", body: "ظاهرة معروفة ويمكن مشاهدتها..", auther: "علي" },
-        { id: 3, title: "لماذا المحاولة", body: "احيانا يمكنك معرفة حيل ال..", auther: "منى" },
-        { id: 4, title: "على ضفاف الشمال", body: "في خيال الشعوب القديمة..", auther: "علي" },
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState("Ahmad");
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        console.log("useEffect is running");
-        // console.log(blogs);
-        console.log(name);
-    }, [name]);
+        setTimeout(() => fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            }), 1000);
+    }, []);
 
 
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="كل التدوينات" handleDelete={handleDelete} />
-            <button onClick={() => setName("Ali")}>غير الاسم</button>
-            <p>{name}</p>
+            {isPending && <div>جاري التحميل...</div>}
+            {blogs && <BlogList blogs={blogs} title="كل التدوينات" />}
         </div>
     );
 }
