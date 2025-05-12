f25409d7af87cafe2e772fbca648413d32bbda5a
Ahmad Owais <okams7@gmail.com>
2025-05-13 01:15:47 +0300

    Forms

--- src/create.js
+++ src/create.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('سالم');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog);
+    }
+
     return (
         <div className="create">
             <h2>اضف مدونة جديدة</h2>
+            <form onSubmit={handleSubmit}>
+                <label>عنوان المدونة:</label>
+                <input type="text" required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>محتوى المدونة:</label>
+                <textarea required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                <label>المؤلف:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="سالم">سالم</option>
+                    <option value="علي">علي</option>
+                    <option value="منى">منى</option>
+                </select>
+
+                <button>اضف مدونة</button>
+            </form>
         </div>
     );
 }
